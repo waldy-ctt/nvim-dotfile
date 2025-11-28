80f5a3a63891750bd8b9a1e21fdd6ac7bce72433
waldy-ctt <clocktoktok@gmail.com>
2025-11-28 13:35:43 +0700

    resolve conflict

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,12 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
-  config = function ()
-      require('conform').setup({
-          formatters_by_ft = {
-              typescript = { "prettier" }
-          }
-      })
-  end
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+			},
+		})
+	end,
 }
